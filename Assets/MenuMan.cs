dfc0b311ab49ec9f9415fb99cc5cf2b491988b47
Zac Lucarelli <chikanz@outlook.com>
2017-02-05 20:28:14 +1100

    Added menu Manager, added speech bubbles and animations

---
+++ Assets/MenuMan.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using InControl;
+using UnityEngine;
+
+public class MenuMan : MonoBehaviour
+{
+    public Animator TitleAnim;
+    public Animator Credit;
+    public Animator SpeechAnim;
+
+    public GameObject[] SpeechBubbles;
+    public int speechCounter = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Invoke("StartMessage", 1);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var controller = InputManager.ActiveDevice;
+
+        if (controller.Action1.WasPressed && speechCounter < SpeechBubbles.Length)
+        {
+            for (int i = 0; i < SpeechBubbles.Length; i++)
+            {
+                SpeechBubbles[i].SetActive(false);
+            }
+            speechCounter++;
+            SpeechBubbles[speechCounter].SetActive(true);
+        }
+    }
+
+    void StartMessage()
+    {
+        SpeechBubbles[speechCounter].SetActive(true);
+    }
+
+    void ExitMenu()
+    {
+        
+    }
+
+
+}
